012b5e112b4b76a0ad473a4887a8c125e1789668
kaitoyama <uitakato@gmail.com>
2024-09-06 01:18:31 +0900

    add db connection and level validation

--- go.mod
+++ go.mod
@@ -8,7 +8,10 @@ require (
 	github.com/traPtitech/traq-ws-bot v1.2.1
 )
 
+require filippo.io/edwards25519 v1.1.0 // indirect
+
 require (
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gofrs/uuid/v5 v5.3.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/sashabaranov/go-openai v1.29.1
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/sashabaranov/go-openai"
@@ -12,6 +14,10 @@ import (
 	traq "github.com/traPtitech/go-traq"
 	traqwsbot "github.com/traPtitech/traq-ws-bot"
 	payload "github.com/traPtitech/traq-ws-bot/payload"
+
+	"database/sql"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 var (
@@ -31,6 +37,31 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Println(err)
+	}
+
+	c := mysql.Config{
+		DBName:    os.Getenv("NS_MARIADB_DATABASE"),
+		User:      os.Getenv("NS_MARIADB_USER"),
+		Passwd:    os.Getenv("NS_MARIADB_PASSWORD"),
+		Addr:      os.Getenv("NS_MARIADB_HOST") + ":" + os.Getenv("NS_MARIADB_PORT"),
+		Net:       "tcp",
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+
+	db, err := sql.Open("mysql", c.FormatDSN())
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS image_proc (id INT AUTO_INCREMENT PRIMARY KEY, user_name VARCHAR(255), level INT, miss_type_count INT, speed INT, accuracy FLOAT)")
 
 	ACCESS_TOKEN := os.Getenv("ACCESS_TOKEN")
 	bot, err = traqwsbot.NewBot(&traqwsbot.Options{
@@ -55,16 +86,38 @@ func main() {
 			}
 		} else {
 			message := image_proc(images[0])
-			log.Println("Received MESSAGE_CREATED event: " + p.Message.Text)
-			_, _, err := bot.API().
-				MessageApi.
-				PostMessage(context.Background(), p.Message.ChannelID).
-				PostMessageRequest(traq.PostMessageRequest{
-					Content: message,
-				}).
-				Execute()
+			// message is like a JSON string
+			// {"user_name":"sashabaranov","level":1,"miss_type_count":0,"speed":0,"accuracy":0}
+			// We can parse it to a struct
+			var score ImageProc
+			err := json.Unmarshal([]byte(message), &score)
 			if err != nil {
 				log.Println(err)
+			} else {
+				if score.Level != 5 {
+					log.Println("Received MESSAGE_CREATED event: " + p.Message.Text)
+					_, _, err = bot.API().
+						MessageApi.
+						PostMessage(context.Background(), p.Message.ChannelID).
+						PostMessageRequest(traq.PostMessageRequest{
+							Content: "おっと、レベル5じゃないとダメだよ!",
+						}).
+						Execute()
+					if err != nil {
+						log.Println(err)
+					}
+				}
+				log.Println("Received MESSAGE_CREATED event: " + p.Message.Text)
+				_, _, err = bot.API().
+					MessageApi.
+					PostMessage(context.Background(), p.Message.ChannelID).
+					PostMessageRequest(traq.PostMessageRequest{
+						Content: fmt.Sprintf("以下の成績で受け付けました。何か誤りがある場合はkaitoyamaをメンションしてください\nユーザー名: %s\nレベル: %d\nミスタイプ数: %d\nスピード: %d\n正確性: %.2f", score.UserName, score.Level, score.MissTypeCount, score.Speed, score.Accuracy),
+					}).
+					Execute()
+				if err != nil {
+					log.Println(err)
+				}
 			}
 		}
 	})
